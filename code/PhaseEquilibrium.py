c55ada42f87e71116ec0f0ea9e2251e0e3b61525
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-06-29 23:14:29 +0300

    сделана первая рабочая версия flash-расчета

--- code/PhaseEquilibrium.py
+++ code/PhaseEquilibrium.py
@@ -1,6 +1,7 @@
 import math as math
 from scipy.optimize import newton, bisect
 from logger import LogManager
+from EOS_PR_v2 import EOS_PR
 from PhaseStability_v3 import PhaseStability
 
 
@@ -38,17 +39,35 @@ class PhaseEquilibrium:
         self.phase_stability.stability_loop()
         self.phase_stability.interpetate_stability_analysis()
 
-        # 
+
+        # В зависимости от результата анализа стабильности определяем дальнейший переход
         if self.phase_stability.stable == True:
             # Здесь переход к расчету свойств системы
-            print('Сразу переходим к расчету свойств ')
+            print('Сразу переходим к расчету свойств')
 
         else:
             # Здесь переход к алгоритму Рэшфорда-Райса
 
             ##TODO: в каких случаях какие Кi берем в качестве исходных?
             self.k_values = self.phase_stability.k_values_vapour
-            #print('Нужен расчет Рэшфорда райса')
+            self.fv = self.find_solve_bisection_v4()
+
+            # Определяем составы жидкой и газовой фазы
+            self.yi_v = self.define_yi_v()
+            self.xi_l = self.define_xi_l()
+
+            # Создаем объекты УРС для решения газовой и жидкой фаз
+
+            self.eos_vapour = EOS_PR(zi = self.yi_v, p = self.p, t = self.t)
+            self.eos_liquid = EOS_PR(zi = self.xi_l, p = self.p, t = self.t)
+
+            # Расчет Ri
+            self.ri = self.calc_Ri(self.eos_vapour, self.eos_liquid)
+
+            # Проверки сходимости
+            self.check_convergence_ri()
+            self.check_trivial_solution()
+
 
 
 
@@ -124,25 +143,138 @@ class PhaseEquilibrium:
         
         return fvv  # Возвращаем последнее приближение, если не достигли tol
 
+    def find_solve_bisection_v4(self, tol=1e-13):
+        fv_min = 0.0  # Минимально возможная доля пара
+        fv_max = 1.0  # Максимально возможная доля пара
+
+        def compute_sum(fvv):
+            total = 0.0
+            for component in self.k_values:
+                K_i = self.k_values[component]
+                z_i = self.zi[component]
+                denominator = 1 + fvv * (K_i - 1)
+                if abs(denominator) < 1e-10:
+                    denominator = 1e-10  # Защита от деления на ноль
+                total += z_i * (K_i - 1) / denominator
+            return total
+
+        # Проверяем, есть ли корень в [0, 1]
+        sum_at_0 = compute_sum(0.0)
+        sum_at_1 = compute_sum(1.0)
+
+        if sum_at_0 * sum_at_1 > 0:
+            raise ValueError("Нет решения в диапазоне fvv ∈ [0, 1]. Проверьте K_i и z_i.")
+
+        # Метод бисекции
+        for _ in range(100):
+            fvv = (fv_min + fv_max) / 2
+            sum_mid = compute_sum(fvv)
+
+            if abs(sum_mid) < tol:
+                return fvv
+
+            if sum_at_0 * sum_mid < 0:
+                fv_max = fvv
+            else:
+                fv_min = fvv
+
+        return fvv
+    
+
 
     # Метод расчета состава газа через найденное Fv
-    def define_yi_v(self, fv):
+    def define_yi_v(self):
         yi_v = {}
         for component in self.zi.keys():
-            yi_v[component] = self.zi[component] * self.k_values[component] / ((fv * (self.k_values[component] - 1) + 1))
+            yi_v[component] = round(self.zi[component] * self.k_values[component] / ((self.fv * (self.k_values[component] - 1) + 1)), 4)
         
 
         return yi_v
 
 
     # Метод расчета состава жидкости через найденное Fv
-    def define_xi_l(self, fv):
+    def define_xi_l(self):
         xi_l = {}
         for component in self.zi.keys():
-            xi_l[component] = self.zi[component] / ((fv * (self.k_values[component] - 1)) + 1)
+            xi_l[component] = round(self.zi[component] / ((self.fv * (self.k_values[component] - 1)) + 1), 4)
         
 
         return xi_l
+
+    # Метод расчета Ri
+    def calc_Ri(self, eos_vapour:EOS_PR, eos_liquid: EOS_PR):
+        ri = {}
+        for component in self.zi.keys():
+            ri[component] = math.exp(eos_liquid.fugacity_by_roots[eos_liquid.choosen_eos_root][component]) / math.exp(eos_vapour.fugacity_by_roots[eos_vapour.choosen_eos_root][component]) 
+        return ri
+
+
+    # Метод проверки сходимости
+    def check_convergence_ri(self, e = math.pow(10,-7)):
+            
+        ri_massive = []
+        for ri in list(self.ri.values()):
+            ri_massive.append((ri-1) ** 2)
+
+        sum_ri = sum(ri_massive)
+
+        if (sum_ri < e):
+            self.convergence = True
+            return True
+        
+        else:
+            self.convergence = False
+            return False
+    
+    
+    # Метод обновления k_values
+    def update_k_values(self):
+        k_vals = {}
+        for component in self.k_values.keys():
+            k_vals[component] = self.k_values[component] * self.ri[component]
+        
+        return k_vals
+
+
+
+    # Метод проверки тривиального решения
+    def check_trivial_solution(self):
+        ln_ki = []
+        for component in self.k_values.keys():
+            ln_ki.append(math.pow(math.log(self.k_values[component]), 2))
+        
+        if sum(ln_ki) < math.pow(10, -4):
+            self.trivial_solution = True
+            return True
+        else:
+            self.trivial_solution = False
+            return False
+
+
+    # Итерационный метод поиска решения
+    def find_solve_loop(self):
+
+        while (self.convergence == False) and (self.trivial_solution == False):
+            
+            self.k_values = self.update_k_values()
+
+            self.fv = self.find_solve_bisection_v4()
+
+            self.yi_v = self.define_yi_v()
+            self.xi_l = self.define_xi_l()
+
+            self.eos_vapour = EOS_PR(zi = self.yi_v, p = self.p, t = self.t)
+            self.eos_liquid = EOS_PR(zi = self.xi_l, p = self.p, t = self.t)
+
+            self.ri = self.calc_Ri(self.eos_vapour, self.eos_liquid)
+
+            self.check_convergence_ri()
+            self.check_trivial_solution()
+
+            print('loop')
+
+        
+
 
 
 
@@ -151,11 +283,9 @@ class PhaseEquilibrium:
         ...
 
 
-
     # Метод расчета шифт-параметра
     def clac_shift(self):
         ...
-
 
 
     # Метод расчета плотности системы
@@ -165,11 +295,17 @@ class PhaseEquilibrium:
 
 
 if __name__ == '__main__':
-    equilibrium = PhaseEquilibrium({'C1': 0.4, 'nC4':0.6}, 5, 40)
-    print(equilibrium.k_values)
+    equilibrium = PhaseEquilibrium({'C1': 0.9, 'nC4':0.1}, 5, 40)
+    
+    print(equilibrium.xi_l)
+    print(equilibrium.yi_v)
 
-    print(equilibrium.find_solve_bisection_v3())
+    equilibrium.find_solve_loop()
 
-    print(equilibrium.define_xi_l(0.2146))
-    print(equilibrium.define_yi_v(0.2146))
+    print(equilibrium.xi_l)
+    print(equilibrium.yi_v)
+
+
+
+
 
