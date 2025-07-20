45d4721396f757b4909f6fe0ba507eacc2c48e6b
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-07-20 23:04:51 +0300

    Реализован расчет Pdew, пока что все отдельными методами.

--- code/calculations/Phase_Diagram_v2.py
+++ code/calculations/Phase_Diagram_v2.py
@@ -4,12 +4,15 @@ import math as math
 
 
 class PhaseDiagram:
+
     def __init__(self, zi:Composition, p_max:float, p_min = 0.1):
         self.zi = zi.composition
         self.p_min = p_min
         self.p_max = p_max
         self.p_i = self.p_max / 2
 
+
+## Часть алгоритма для расчета Pb
     def define_s_sp(self, p, t):
         '''
         метод позволяет определить соотношения Sv и Sl при заданных PT.
@@ -85,8 +88,7 @@ class PhaseDiagram:
         return {'s_sp':S_sp, 'y_sp':y_sp, 'k_sp':k_sp, 'r_sp':r_sp, 'letuch_sp':letuch_sp, 'letuch_z': letuch_z}
 
 
-
-    def main_loop(self, t, lambd = 1):
+    def main_loop_sp(self, t, lambd = 1):
         cur_s_sp = self.define_s_sp(self.p_i, t =t)
 
         # Если s_sp 0, то обновляем давление
@@ -150,17 +152,170 @@ class PhaseDiagram:
                 print('Pb не найдено')
                 print(cur_s_sp)
                 print(self.p_i)
-                
+
+
     def loop_v2(self, t):
-        self.main_loop(t)
+        self.main_loop_sp(t)
         
         while ((abs(1 - self.sum_y_sp) < math.pow(10, -3)) == False) and ((math.pow(self.Ykz,2) < math.pow(10,-3)) == False):
-            self.main_loop(t)
+            self.main_loop_sp(t)
+        
+        self.p_i = self.p_i / 2
 
 
 
+## Часть алгоритма для расчета Pdew
 
-    
+    def define_s_dp(self, p, t):
+        '''
+        метод позволяет определить соотношения Sv и Sl при заданных PT.
+        Вспомогательный метод для алгоритма
+        
+        '''
+        phase_stability = PhaseStability(self.zi, p, t)
+
+        if (phase_stability.S_l - 1) < 10 ** -5:
+            if (phase_stability.S_v) < 10 ** -5:
+                y_dp = {}
+                for component in self.zi.keys():
+                    y_dp[component] = 0
+                k_dp = None
+                r_dp = None
+                letuch_z = None
+                letuch_dp = None
+
+        
+        if phase_stability.S_l > 1:
+            if phase_stability.S_l > phase_stability.S_v:
+                k_dp = phase_stability.k_values_liquid
+                r_dp = phase_stability.ri_l
+                letuch_z = phase_stability.initial_eos.fugacity_by_roots[phase_stability.initial_eos.choosen_eos_root]
+                letuch_dp = phase_stability.liquid_eos.fugacity_by_roots[phase_stability.liquid_eos.choosen_eos_root]
+                y_dp = {}
+                for component in self.zi.keys():
+                    y_dp[component] = self.zi[component] / phase_stability.k_values_liquid[component]
+            
+            else:
+                k_dp = phase_stability.k_values_vapour
+                r_dp = phase_stability.ri_v
+                letuch_z = phase_stability.initial_eos.fugacity_by_roots[phase_stability.initial_eos.choosen_eos_root]
+                letuch_dp = phase_stability.vapour_eos.fugacity_by_roots[phase_stability.vapour_eos.choosen_eos_root]
+                y_dp = {}
+                for component in self.zi.keys():
+                    y_dp[component] = self.zi[component] * phase_stability.k_values_vapour[component]
+        
+        else:
+                if phase_stability.S_v < 1:
+                    y_dp = {}
+                    for component in self.zi.keys():
+                        y_dp[component] = 0
+                    k_dp = None
+                    r_dp = None
+                    letuch_z = None
+                    letuch_dp = None
+
+        if phase_stability.S_v > 1:
+            if phase_stability.S_v > phase_stability.S_l:
+                k_dp = phase_stability.k_values_vapour
+                r_dp = phase_stability.ri_v
+                letuch_z = phase_stability.initial_eos.fugacity_by_roots[phase_stability.initial_eos.choosen_eos_root]
+                letuch_dp = phase_stability.vapour_eos.fugacity_by_roots[phase_stability.vapour_eos.choosen_eos_root]
+                y_dp = {}
+                for component in self.zi.keys():
+                    y_dp[component] = self.zi[component] * phase_stability.k_values_vapour[component]
+            
+            else:
+                if phase_stability.S_l < 1:
+                    y_dp = {}
+                    for component in self.zi.keys():
+                        y_dp[component] = 0
+                    k_dp = None
+                    r_dp = None
+                    letuch_z = None
+                    letuch_dp = None
+
+        S_dp = sum(y_dp.values())
+        #print(S_sp)
+        print(f'S_v: {phase_stability.S_v}, S_l: {phase_stability.S_l}')
+        print(phase_stability.stable)
+        return {'s_dp':S_dp, 'y_dp':y_dp, 'k_dp':k_dp, 'r_dp':r_dp, 'letuch_dp':letuch_dp, 'letuch_z': letuch_z}
+
+    def main_loop_dp(self, t, lambd = 1):
+        self.p_min = 0.1
+        
+        #TODO: Проблема в этом. Пробуем исправить подменой Pi после определения Pb
+        #self.p_i = self.p_i / 2
+        
+        cur_s_dp = self.define_s_dp(self.p_i, t =t)
+
+        # Если s_dp 0, то обновляем давление
+        while cur_s_dp['s_dp'] == 0:
+            print(self.p_i)
+            self.p_min = self.p_i
+            self.p_i = (self.p_max + self.p_min) / 2
+            
+            # Проверка на то, что P_max и P_min не равны друг другу
+            if self.p_max - self.p_min == math.pow(10,-8):
+                print('P_max и P_min равны, давление насыщения не найдено!!!')
+            
+            # Считаем s_sp еще раз
+            else:
+                cur_s_dp = self.define_s_dp(self.p_i, t)
+                print(self.p_i)
+        # если ssp не ноль, то начинается цикл  расчета Pdew
+        else:
+
+            # не разобрался зачем это есть в вба
+            r_sp1 = cur_s_dp['r_dp']
+
+
+            r_dp = {}
+            for component in cur_s_dp['letuch_z'].keys():
+                r_dp[component] = math.exp(cur_s_dp['letuch_z'][component]) / (math.exp(cur_s_dp['letuch_dp'][component]) * cur_s_dp['s_dp'])
+            
+            y_dp = {}
+            for component in r_dp.keys():
+                y_dp[component] = cur_s_dp['y_dp'][component] * math.pow(r_dp[component], lambd)
+            print(f'y_dp: {y_dp}')
+
+            
+            self.sum_y_dp = sum(y_dp.values())
+            print(f'sum_y_dp: {self.sum_y_dp}')
+            print(f'sum_y_dp-1: {abs(self.sum_y_dp - 1)}')
+
+
+
+            #строки с 559 в вба
+            self.Sum_dp = []
+            for component in self.zi:
+                self.Sum_dp.append(math.log(r_dp[component]) / math.log(y_dp[component]) / self.zi[component])
+            self.Sum_dp = sum(self.Sum_dp)
+
+            self.Ykz_dp = []
+            for component in self.zi:
+                self.Ykz_dp.append(y_dp[component] / self.zi[component])
+            self.Ykz_dp = sum(self.Ykz_dp)
+
+            if abs(1 - self.sum_y_dp) < math.pow(10, -3):
+                print(f'Pdew найдено: {self.p_i}')
+
+            elif math.pow(self.Ykz_dp,2) < math.pow(10,-3):
+                print(f'Pdew найдено: {self.p_i}')
+
+            else:
+                print(self.p_i)
+                self.p_max = self.p_i
+                self.p_i = (self.p_max + self.p_min) / 2
+                print('Pdew не найдено')
+                print(cur_s_dp)
+                print(self.p_i)
+
+    def loop_v2_dew(self, t):
+        self.main_loop_dp(t)
+        
+        while ((abs(1 - self.sum_y_dp) < math.pow(10, -3)) == False) and ((math.pow(self.Ykz_dp,2) < math.pow(10,-3)) == False):
+            self.main_loop_dp(t)
+
 
 
 
@@ -168,6 +323,7 @@ class PhaseDiagram:
 if __name__ == '__main__':
     comosition = Composition({'C1': 0.6, 'nC4':0.4})
     phase_diag = PhaseDiagram(comosition, 40)
-    phase_diag.loop_v2(350) 
+    phase_diag.loop_v2(345) 
+    phase_diag.loop_v2_dew(345)
 
 
