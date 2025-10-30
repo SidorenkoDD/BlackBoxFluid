d6927398eadf1e75398d1577c40200b5022774fd
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-10-30 23:30:45 +0300

    Начал добавлять LBC

--- code/calculations/Utils/Viscosity.py
+++ code/calculations/Utils/Viscosity.py
@@ -1,2 +1,12 @@
-class Viscosity:
-    ...
+from calculations.Composition.Composition import Composition
+
+
+class LBC:
+    def __init__(self, param_set: dict = {'alpha0' : 0.1023,
+                                          'alpha1' : 0.023364,
+                                          'alpha2' : 0.058533,
+                                          'alpha3' : -0.040758,
+                                          'alpha4' : 0.0093324}):
+        self.param_set = param_set
+
+    
