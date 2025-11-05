b843d666071e6f1b06554b6b46048bcca8dfdbb1
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-11-05 18:02:10 +0300

    добавлена часть расчета ССЕ V/Vsat

--- code/calculations/Experiments/CCE.py
+++ code/calculations/Experiments/CCE.py
@@ -1,10 +1,11 @@
-import numpy as np
+import pandas as pd
 from calculations.Experiments.BaseExperiment import PVTExperiment
 from calculations.VLE.flash import FlashFactory
 from calculations.PhaseDiagram.SaturationPressure import SaturationPressureCalculation
 from calculations.Utils.Conditions import Conditions
 from calculations.Utils.Errors import InvalidPressureSequence
 from calculations.Utils.Results import CCEResults
+
 
 
 class CCE(PVTExperiment):
@@ -15,47 +16,56 @@ class CCE(PVTExperiment):
 
 
 
-    def calculate(self, p_start : float, temperature : float, pressure_list : list | None = None,
-                   n_steps : float = 10, flash_type = 'TwoPhaseFlash'):
+    def calculate(self, p_resirvoir : float,
+                  temperature : float,
+                  pressure_by_stages : list,
+                   flash_type = 'TwoPhaseFlash'):
         '''Method calculates CCE between p_start and Pbub with constant temperature.
         
         By default, interpolation between p_start and Pb (automatically calculated) for n_steps
         
         Args
         ----
-        * p_start - first stage pressure.
+        * p_resirvoir - reservoir pressure
         * temperature - constant temperature for experiment
-        * pressure_list - optional to define pressure stages
-        * n_steps - points for interpolation, by default 10
+        * pressure_by_stages - list with pressure stages
         '''
         result = {}
-        if pressure_list is None:
-            pb_obj = SaturationPressureCalculation(self._composition,p_max=50, temp= temperature)
-            pb = pb_obj.sp_convergence_loop(self._eos)
-            pressure_array = np.linspace(p_start, pb, n_steps)
-            for p in pressure_array:
+
+        def _is_strictly_descending() -> bool:
+            '''Method checks descending values for pressure_list'''
+            return all(pressure_by_stages[i] < pressure_by_stages[i-1] for i in range(1, len(pressure_by_stages)))
+
+
+        def _is_p_pres_in_pressure_by_stages_list() -> list:
+            '''Method checks is p_res in list, in no, append p_res in list'''
+
+            if p_resirvoir in pressure_by_stages:
+                pass
+            else:
+                pressure_by_stages.append(p_resirvoir)
+                pressure_by_stages.sort(reverse=True)
+
+        if _is_strictly_descending():
+            _is_p_pres_in_pressure_by_stages_list()
+            for p in pressure_by_stages:
                 current_conditions = Conditions(p, temperature)
                 flash_object = FlashFactory(self._composition, self._eos)
                 flash_calculator = flash_object.create_flash(flash_type= flash_type)
                 result[current_conditions.p] = flash_calculator.calculate(conditions=current_conditions)
         else:
-            def _is_strictly_descending() -> bool:
-                '''Method checks descending values for pressure_list'''
-                return all(pressure_list[i] < pressure_list[i-1] for i in range(1, len(pressure_list)))
-            
-            if _is_strictly_descending():
-                for p in pressure_list:
-                    current_conditions = Conditions(p, temperature)
-                    flash_object = FlashFactory(self._composition, self._eos)
-                    flash_calculator = flash_object.create_flash(flash_type= flash_type)
-                    result[current_conditions.p] = flash_calculator.calculate(conditions=current_conditions)
-            else:
-                raise InvalidPressureSequence(f'pressure_list must be descending only : {pressure_list}')
+            raise InvalidPressureSequence(f'pressure_list must be descending only : {pressure_by_stages}')
 
         self.result = CCEResults(pressure = list(result.keys()),
                                  temperature= temperature,
                                  liquid_volume = [result[x].liquid_volume for x in list(result.keys())],
                                  liquid_density = [result[x].liquid_density for x in list(result.keys())])
+
+        self.dataframe = pd.DataFrame({'Pressure' : self.result.pressure,
+                                       'Liquid volume' : self.result.liquid_volume,
+                                       'Liquid density' : self.result.liquid_density})
+
+        self._calculate_v_d_vpres(p_reservoir=p_resirvoir)
 
         return self.result
 
@@ -64,9 +74,9 @@ class CCE(PVTExperiment):
         ...
 
 
-    def _calculate_v_d_vpres(self):
-        ...
+    def _calculate_v_d_vpres(self, p_reservoir):
+        self.dataframe['V/Vres'] = self.dataframe['Liquid volume'] / self.dataframe[self.dataframe['Pressure'] == p_reservoir]['Liquid volume'].iloc[0]
 
 
-    def _calculate_v_d_vpsat(self):
-        ...
+    def _calculate_v_d_vpsat(self, p_sat):
+        self.dataframe['V/Vres'] = self.dataframe['Liquid volume'] / self.dataframe[self.dataframe['Pressure'] == p_sat]['Liquid volume'].iloc[0]
