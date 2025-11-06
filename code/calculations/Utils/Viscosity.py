1ab0a2e15f188d2078c3abc11a00beff89899753
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-11-06 17:21:19 +0300

    начал класс для LBC

--- code/calculations/Utils/Viscosity.py
+++ code/calculations/Utils/Viscosity.py
@@ -1,12 +1,34 @@
-from calculations.Composition.Composition import Composition
+'''Module for viscosity calculations'''
+
+from abc import ABC
 
 
-class LBC:
-    def __init__(self, param_set: dict = {'alpha0' : 0.1023,
+class Viscosity(ABC):
+    '''Abstract class to calculate viscosity'''
+    def calculate(self):
+        
+        pass
+
+
+class LBC(Viscosity):
+    '''Class with LBC-method'''
+    def __init__(self, mole_fractions: dict, composition_data, param_set: dict = {'alpha0' : 0.1023,
                                           'alpha1' : 0.023364,
                                           'alpha2' : 0.058533,
                                           'alpha3' : -0.040758,
                                           'alpha4' : 0.0093324}):
         self.param_set = param_set
+        self.mole_fractions = mole_fractions
+        self.composition_data = composition_data
 
-    
+    def _calculate_epsilon_parameter(self):
+        ...
+
+    def _calculate_mu0_i_parameter(self):
+        ...
+
+    def _calculate_mu0_parameter(self):
+        ...
+
+    def calculate(self):
+        ...
