98caf6baf783c6f99258a899a33ad32e51bc9dff
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-11-12 23:35:59 +0300

    добавлен расчет стандартной сепарации, вроде все честно

--- code/calculations/Experiments/StandardSeparation.py
+++ code/calculations/Experiments/StandardSeparation.py
@@ -7,13 +7,34 @@ class StandardSeparation(PVTExperiment):
     def __init__(self, composition, eos):
         self._composition = composition
         self._eos = eos
-        ...
+        self._reservoir_flash_result = None
+        self._stc_flash_result = None
+        self._liquid_volume_res = None
+        self._liquid_volume_stc = None
+        self._gas_vol_stc = None
 
-    def calculate(self, flash_type = 'TwoPhaseFlash'):
+
+    def _calculate_bo(self):
+        self._liquid_volume_res = self._reservoir_flash_result.liquid_volume
+        self._liquid_volume_stc = self._stc_flash_result.liquid_volume
+        return self._liquid_volume_res / self._liquid_volume_stc
+
+    def _calcluate_rs(self):
+        self._gas_vol_stc = self._stc_flash_result.vapour_volume
+        return self._gas_vol_stc / self._liquid_volume_stc
+
+    def calculate(self, p_res: float, t_res:float, flash_type = 'TwoPhaseFlash'):
         self._flash_object = FlashFactory(self._composition, self._eos)
-        self._standard_conditions = Conditions(0.1, 20)
+        self._standard_conditions = Conditions(p_res, t_res)
         flash_calculator = self._flash_object.create_flash(flash_type= flash_type)
-        self._standard_separation_result = flash_calculator.calculate(conditions=self._standard_conditions)
+        self._reservoir_flash_result = flash_calculator.calculate(conditions=self._standard_conditions)
+
+        self._flash_object = FlashFactory(self._composition, self._eos)
+        self._standard_conditions = Conditions(0.101325, 20)
+        flash_calculator = self._flash_object.create_flash(flash_type= flash_type)
+        self._stc_flash_result = flash_calculator.calculate(conditions=self._standard_conditions)
+
+        self._bo = self._calculate_bo()
 
     @property
     def show_results(self):
