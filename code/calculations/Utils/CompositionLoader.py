10186ef14a66e190b5bb1659c3f25aabe702a916
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-12-08 02:09:22 +0300

    добавлена логика для наличия/отсутствия шапки в датафрейме состава

--- code/calculations/Utils/CompositionLoader.py
+++ code/calculations/Utils/CompositionLoader.py
@@ -8,7 +8,6 @@ import pandas as pd
 class CompositionLoader(ABC):
     def __init__(self, filepath):
         self._filepath = filepath
-        self.load()
     @abstractmethod
     def load(self):
         pass
@@ -17,20 +16,24 @@ class CompositionLoader(ABC):
 class CompositionExcelLoader(CompositionLoader):
     def __init__(self, filepath):
         super().__init__(filepath)
-        self.load()
 
-    def load(self):
-        ##TODO: может быть ошибка, если header есть, а предполагается, что его нет
-        df = pd.read_excel(io = self._filepath,
-                           header = None)
+    def load(self, header: bool):
+        if header:
+            df = pd.read_excel(io = self._filepath)
+            df.columns = ["key", "value"]
+            if not pd.api.types.is_numeric_dtype(df['value']):
+                raise InvalidExcelValueType('Value not numeric in Excel!')
+            if not pd.api.types.is_string_dtype(df['key']):
+                raise InvalidExcelComponentType('Value not string in Excel!')
+            dict_from_df = df.set_index('key')['value'].to_dict()
+        else:
+            df = pd.read_excel(io = self._filepath,
+                            header = None)
+            df.columns = ["key", "value"]
+            if not pd.api.types.is_numeric_dtype(df['value']):
+                raise InvalidExcelValueType('Value not numeric in Excel!')
+            if not pd.api.types.is_string_dtype(df['key']):
+                raise InvalidExcelComponentType('Value not string in Excel!')
+            dict_from_df = df.set_index('key')['value'].to_dict()
 
-        df.columns = ["key", "value"]
-
-        if not pd.api.types.is_numeric_dtype(df['value']):
-            raise InvalidExcelValueType('Value not numeric in Excel!')
-
-        if not pd.api.types.is_string_dtype(df['key']):
-            raise InvalidExcelComponentType('Value not string in Excel!')
-
-        dict_from_df = df.set_index('key')['value'].to_dict()
         return dict_from_df
