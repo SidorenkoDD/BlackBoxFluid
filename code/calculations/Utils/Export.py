0a354f36f52f589af115a3c25bdf81cdbfc95840
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-12-21 23:54:40 +0300

    баг с выводом BIC, надо убрать

--- code/calculations/Utils/Export.py
+++ code/calculations/Utils/Export.py
@@ -28,7 +28,7 @@ class E300(Export):
         fname.write('--Equation of state\n')
         fname.write('--\n')
         if self._model._eos == 'PREOS':
-            fname.write('PRCORR\n')
+            fname.write('MPR\n')
         elif self._model._eos == 'SRKEOS':
             fname.write('SRK\n')
         fname.write('/\n')
@@ -106,17 +106,17 @@ class E300(Export):
         fname.write('/\n')
 
     def _write_bic(self, fname):
-        bic_df = self._model._composition.BIPS
+        bic_df = self._model._composition.BIPS.index[1:]
         mask = np.tril(np.ones_like(bic_df))
         bic_df_diag = bic_df.where(mask.astype(bool))
         fname.write('BIC\n')
         fname.write('--\n')
         fname.write('--Binary interaction coefficients\n')
         fname.write('--\n')
-        for row in bic_df_diag.index:
+        for row in bic_df_diag:
             arr = bic_df_diag.loc[row].to_numpy()
             cleared_arr = arr[~np.isnan(arr)]
-            result_string = ' '.join(map(str, cleared_arr))
+            result_string = '  '.join(map(str, cleared_arr))
             fname.write(f'{result_string}\n')
 
 
